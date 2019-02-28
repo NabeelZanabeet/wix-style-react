b7d8e4b41fa0afa84acc78b7a6385555cc20307a
mykas <mykolass@wix.com>
2019-02-28 09:35:18 +0200

    <TextButton/> - initial docs refactor

---
+++ stories/TextButton/examples.js
@@ -0,0 +1,23 @@
+export const underlineNone = `
+<Layout cols={4} gap="10px">
+  <TextButton>standard</TextButton>
+  <TextButton skin="light">light</TextButton>
+  <TextButton skin="premium">premium</TextButton>
+  <TextButton skin="dark">dark</TextButton>
+</Layout>
+`;
+
+export const underlineOnHover = `
+<Layout cols={4} gap="10px">
+  <TextButton underline="onHover">standard</TextButton>
+  <TextButton skin="light" underline="onHover">
+    light
+  </TextButton>
+  <TextButton skin="premium" underline="onHover">
+    premium
+  </TextButton>
+  <TextButton skin="dark" underline="onHover">
+    dark
+  </TextButton>
+</Layout>
+`;
--- stories/TextButton/index.story.js
+++ stories/TextButton/index.story.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import {
+  tab,
+  tabs,
+  api,
+  playground,
+  description,
+  divider,
+  importExample,
+  columns,
+  header,
+  code as baseLiveCode,
+} from 'wix-storybook-utils/Sections';
+
 import TextButton from 'wix-style-react/TextButton';
+import { Layout } from 'wix-style-react/Layout';
 import { storySettings } from './storySettings';
 import icons from '../utils/icons-for-story';
+import { baseScope } from '../utils/Components/LiveCodeExample';
+import testkit from '!raw-loader!./testkit.md';
+import * as examples from './examples';
+import styles from './examples.scss';
 
-import TextButtonStory from './examples';
+const liveCode = config =>
+  baseLiveCode({
+    previewProps: {
+      className: styles.livePreview,
+    },
+    components: baseScope,
+    ...config,
+  });
+
+const example = ({ title, source }) =>
+  liveCode({
+    title,
+    source,
+  });
 
 export default {
   category: storySettings.kind,
@@ -27,5 +58,69 @@ export default {
     as: ['button', 'a', 'span', 'div'],
   },
 
-  examples: <TextButtonStory />,
+  sections: [
+    header({
+      component: (
+        <Layout gap={0}>
+          <TextButton>Text Button</TextButton>
+        </Layout>
+      ),
+      sourceUrl:
+        'https://github.com/wix/wix-style-react/tree/master/src/TextButton/TextButton.js',
+      issueUrl: 'https://github.com/wix/wix-style-react/issues/new',
+    }),
+    tabs({
+      tabs: [
+        tab({
+          title: 'Description',
+          sections: [
+            description({
+              text: 'Text buttons minimize distraction from content.',
+            }),
+
+            importExample({
+              source: "import TexButton from 'wix-style-react/TextButton';",
+            }),
+
+            divider(),
+
+            columns({
+              items: [
+                description({
+                  text: '### Examples',
+                }),
+                description(),
+              ],
+            }),
+
+            ...[
+              {
+                title: 'Underline: none',
+                source: examples.underlineNone,
+              },
+              {
+                title: 'Underline: onHover',
+                source: examples.underlineOnHover,
+              },
+            ].map(example),
+          ],
+        }),
+
+        tab({
+          title: 'API',
+          sections: [api()],
+        }),
+
+        tab({
+          title: 'Testkit',
+          sections: [description({ text: testkit })],
+        }),
+
+        tab({
+          title: 'Playground',
+          sections: [playground()],
+        }),
+      ],
+    }),
+  ],
 };
