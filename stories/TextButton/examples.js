6803e9554c78fe7803c56caa2e7d90471a7ef266
mykas <mykolass@wix.com>
2019-02-28 11:19:45 +0200

    <TextButton/> - delete example files and add it to sections

--- stories/TextButton/examples.js
+++ stories/TextButton/examples.js
@@ -1,5 +1,5 @@
 export const underlineNone = `
-<Layout cols={4} gap="10px">
+<Layout cols={4} gap={0}>
   <TextButton>standard</TextButton>
   <TextButton skin="light">light</TextButton>
   <TextButton skin="premium">premium</TextButton>
@@ -8,7 +8,7 @@ export const underlineNone = `
 `;
 
 export const underlineOnHover = `
-<Layout cols={4} gap="10px">
+<Layout cols={4} gap={0}>
   <TextButton underline="onHover">standard</TextButton>
   <TextButton skin="light" underline="onHover">
     light
@@ -18,6 +18,49 @@ export const underlineOnHover = `
   </TextButton>
   <TextButton skin="dark" underline="onHover">
     dark
+  </TextButton>
+</Layout>
+`;
+
+export const underlineAlways = `
+<Layout cols={4} gap={0}>
+  <TextButton underline="always">standard</TextButton>
+  <TextButton skin="light" underline="always">
+    light
+  </TextButton>
+  <TextButton skin="premium" underline="always">
+    premium
+  </TextButton>
+  <TextButton skin="dark" underline="always">
+    dark
+  </TextButton>
+</Layout>
+`;
+
+export const affixes = `
+<Layout cols={4} gap={0}>
+  <TextButton prefixIcon={<ChevronDown />}>prefix</TextButton>
+  <TextButton suffixIcon={<ChevronDown />}>suffix</TextButton>
+</Layout>
+`;
+
+export const size = `
+<Layout cols={4} gap={0}>
+  <TextButton size="small">
+    small
+  </TextButton>
+  <TextButton size="medium">
+    medium
+  </TextButton>
+</Layout>`;
+
+export const custom = `
+<Layout cols={4} gap={0}>
+  <TextButton as="a">
+      HTML a
+  </TextButton>
+  <TextButton as={Link} skin="premium">
+      React Router Link
   </TextButton>
 </Layout>
 `;
