1e6a9da83d3a98f87e52a2a98c155e00914182f9
Erez Makavy <erezm@wix.com>
2019-03-03 22:34:15 +0200

    minor: Change TestStories test name

--- scripts/component-generator/templates/component/stories/components/Component/ComponentTestStories.js
+++ scripts/component-generator/templates/component/stories/components/Component/ComponentTestStories.js
@@ -7,7 +7,7 @@ import {%ComponentName%} from '../../../src/{%ComponentName%}';
 
 const TestStories = storiesOf(getTestStoryKind(storySettings), module);
 
-TestStories.add('1. Test One', () => (
+TestStories.add('Test One', () => (
   <{%ComponentName%}
     dataHook="story-{%component-name%}-test-one"
     buttonText="Press me for a surprise"
